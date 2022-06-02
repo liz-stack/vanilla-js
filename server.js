b87433a63dca943406c5a6db42b9144f581469e5
liz-stack <yeoju.kook@gmail.com>
2022-06-03 01:58:50 +0900

    cors 제거(필요없었음)

--- server.js
+++ server.js
@@ -1,27 +1,12 @@
-const http = require('http');
-const port = 5000;
+const express = require("express");
+const path = require("path");
 
-const users = {};
+//express app 생성
+const app = express();
 
-http.createServer((req, res) => {
-    const headers = {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': 'OPTIONS, POST, GET',
-        'Access-Control-Max-Age': 259200, //30일 
-    };
+app.get("/*", (req, res) => {
+    express.static(path.resolve(__dirname, "frontend", "static"),
+        { extensions: ["js"] });
+});
 
-    if (req.method === 'OPTIONS') {
-        res.writeHead(204, headers);
-        res.end();
-        return;
-    }
-
-    if (['GET', 'POST'].indexOf(req.method) > -1) {
-        res.writeHead(200, headers);
-        res.end('Hello World');
-        return;
-    }
-
-    res.writeHeade(405, headers);
-    res.end(`${req.method} is not allowed for request.`)
-}).listen(port);
+app.listen(process.env.PORT || 3000, () => console.log("Server running..."));
