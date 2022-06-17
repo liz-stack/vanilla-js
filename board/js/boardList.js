f03ee7a122324d4fd72b67a8073d2ccbd0c63006
liz-stack <yeoju.kook@gmail.com>
2022-06-18 02:53:38 +0900

    페이징 prev, next

--- board/js/boardList.js
+++ board/js/boardList.js
@@ -1,27 +1,31 @@
+var listURL = "https://2ee5455b-7faf-452a-a894-1463b6252065.mock.pstmn.io/api/boards/free";
+var boardList = [];
 
-function getBoardList() {
-    fetch("https://2ee5455b-7faf-452a-a894-1463b6252065.mock.pstmn.io/api/boards/free").then((data) => {
-        return data.json();
-    }).then((res) => {
-        console.log(res[0].boardId);
-        let boardCount = 0;
-        let boardList = "";
-        res.map((values) => {
-            boardList += `<tr>
-        <td>${values.category}</td>
-        <td><a href="/boards/detail?id=${values.boardId}>${values.title}</a></td>
-        <td>${values.userName}</td>
-        <td>${values.createDate}</td>
-        <td>${values.modifyDate}</td>
-        <td>${values.viewCount}</td>
-        </tr>`;
-        })
-        document.getElementById("boardList").innerHTML = boardList;
-    }).catch((err) => {
-        console.log(err);
-    })
+let pageSize = 10;
+let currentPage = 1;
 
+function previousPage() {
+    if (currentPage > 1)
+        currentPage--;
+    renderTable(currentPage)
+}
+function nextPage() {
+    if ((currentPage * pageSize) < boardList.length)
+        currentPage++;
+    renderTable(currentPage)
+}
+
+function numberOfPages() {
+    return Math.ceil(boardList.length / pageSize)
+}
+
+document.querySelector('#prevBtn').addEventListener('click', previousPage, false)
+document.querySelector('#nextBtn').addEventListener('click', nextPage, false)
+
+async function getBoardList() {
+    const res = await fetch(`${listURL}`)
+    const lists = await res.json()
+    boardList = lists
+    console.log(list);
 };
-
-
-getBoardList();
+getBoardList()
