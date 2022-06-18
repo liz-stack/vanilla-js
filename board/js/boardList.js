50de93009a062d4f1d99ae7a38001bb048f91b68
liz-stack <yeoju.kook@gmail.com>
2022-06-18 12:20:33 +0900

    페이징 <,>

--- board/js/boardList.js
+++ board/js/boardList.js
@@ -1,31 +1,77 @@
 var listURL = "https://2ee5455b-7faf-452a-a894-1463b6252065.mock.pstmn.io/api/boards/free";
-var boardList = [];
 
-let pageSize = 10;
-let currentPage = 1;
+var listData = [];
+var listCount = 0;
+
+const pageSize = 10;
+let curPage = 1;
+
+
+async function renderTable(page = 1) {
+    await getBoardList()
+
+    if (page == 1) {
+        prevBtn.style.visibility = "hidden"
+    } else {
+        prevBtn.style.visibility = "visible"
+    }
+
+    if (page == numberOfPages) {
+        nextBtn.style.visibility = "hidden"
+    } else {
+        nextBtn.style.visibility = "visible"
+    }
+
+
+    // create html
+    var trHtml = "";
+    listData.filter((row, index) => {
+        let start = (curPage - 1) * pageSize;
+        let end = curPage * pageSize;
+        if (index >= start && index < end) return true;
+    }).forEach(list => {
+        trHtml += "<tr>";
+        trHtml += `<td> ${list.category}</td>`;
+        trHtml += `<td> ${list.title} </td>`;
+        trHtml += `<td> ${list.userName}</td>`;
+        trHtml += `<td> ${list.createDate}</td>`;
+        trHtml += `<td> ${list.modifyDate}</td>`;
+        trHtml += `<td> ${list.viewCount}</td>`;
+        "<tr>";
+    });
+    document.getElementById("list").innerHTML = trHtml;
+}
+
+
 
 function previousPage() {
-    if (currentPage > 1)
-        currentPage--;
-    renderTable(currentPage)
+    if (curPage > 1)
+        curPage--;
+    renderTable(curPage)
 }
 function nextPage() {
-    if ((currentPage * pageSize) < boardList.length)
-        currentPage++;
-    renderTable(currentPage)
+    if ((curPage * pageSize) < listData.length)
+        curPage++;
+    renderTable()
 }
+renderTable()
 
 function numberOfPages() {
-    return Math.ceil(boardList.length / pageSize)
+    return Math.ceil(listData.length / pageSize)
 }
+
+
 
 document.querySelector('#prevBtn').addEventListener('click', previousPage, false)
 document.querySelector('#nextBtn').addEventListener('click', nextPage, false)
 
+
+//Fetch Data from API
 async function getBoardList() {
-    const res = await fetch(`${listURL}`)
-    const lists = await res.json()
-    boardList = lists
-    console.log(list);
-};
-getBoardList()
+    const response = await fetch(listURL)
+    const json = await response.json()
+    listData = json.boardList
+    listCount = json.boardList.length
+    console.log(listData)
+    console.log(listCount)
+}
